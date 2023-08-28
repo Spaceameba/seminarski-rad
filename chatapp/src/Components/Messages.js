4dc68344c25cdb2c1e553cbae83bc1af1f1abd19
Spaceameba <barbara.jurkovic@gmail.com>
2023-08-28 21:34:17 +0200

    implement scaledrone

---
+++ chatapp/src/Components/Messages.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class Messages extends Component {
+  render() {
+    const { messages } = this.props;
+    return (
+      <ul className="MessageLog">
+        {messages.map((m) => this.renderMessage(m))}
+      </ul>
+    );
+  }
+  renderMessage(message) {
+    const { member, data, id } = message;
+    const { currentMember } = this.props;
+    const { username, color } = member.clientData;
+    const messageFromMe = member.id === currentMember.id;
+    const className = messageFromMe
+      ? "Messages-message currentMember"
+      : "Messages-message";
+    return (
+      <li key={id} className={className}>
+        <span className="avatar" style={{ backgroundColor: color }} />
+        <div className="Message-content">
+          <div className="username">{username}</div>
+          <div className="text">{data}</div>
+        </div>
+      </li>
+    );
+  }
+}
+
+export default Messages;
